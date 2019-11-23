3d17ee28eea8c3b31ee7eb01cb1fd4c09d57ee5c
David Ellis <dgellis@live.com>
2019-11-23 11:08:03 -0700

    Initial commit

---
+++ animalData.js
@@ -0,0 +1,14 @@
+var animalData = [
+    {
+        name: "dog",
+        pictureURL: "https://www.washingtonpost.com/resizer/kPkFQsQjvSIjfXG-mFXDEpxq6-4=/767x0/smart/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/HB4AT3D3IMI6TMPTWIZ74WAR54.jpg"
+    },
+    {
+        name: "cat",
+        pictureURL: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-cat-wearing-sunglasses-while-sitting-royalty-free-image-1571755145.jpg?crop=0.670xw:1.00xh;0.147xw,0&resize=480:*"
+    },
+    {
+        name: "fish",
+        pictureURL: "https://usercontent1.hubstatic.com/14148036_f520.jpg"
+    }
+]
